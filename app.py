0fd1f2b28ffd740701e48fed05fd8cea802793aa
ferrysany <dancsyip@gmail.com>
2026-04-04 13:17:36 +0800

    5 Mar update

--- app.py
+++ app.py
@@ -9,7 +9,12 @@ if st.button('🔄 Run My Script'):
     result = subprocess.run([sys.executable, 'main.py'], capture_output=True, text=True)
 
     if result.returncode == 0:
-        st.markdown(result.stdout.replace('\n',' \n'))
+        #st.markdown(result.stdout.replace('\n',' \n'))
+        # Split the output by lines and display each line separately
+        for line in res.stdout.split('\n'):
+            if line.strip():
+                # st.write is natively responsive and adds proper spacing
+                st.write(line.strip())
     else:
         st.error("❌ Your script crashed. Here is why:")
         st.code(result.stderr)  # This is the "magic" line that shows the bug
